f01cf46eaa14336e08ad571ebcc0aa37c57f54c9
Wilson Kinyua <wilsonkinyuam@gmail.com>
2022-10-11 17:21:50 +0300

    sidebar navigation added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          {/* <img
-            src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png"
-            alt="Logo"
-          /> */}
+    <>
+      <div className="side-nav">
+        <Link className="nav-close">
+          <FontAwesomeIcon icon={faXmark} />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-          <div className="d-flex">
+        <div className="container">
+          <div className="side-logo">
+            <Link to="/">
+              <img
+                src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png"
+                alt="Logo"
+              />
+            </Link>
+          </div>
+          <div className="side-menu">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link to="/" className="nav-link">
-                  Start Here
+                  Debt management
                 </Link>
               </li>
               <li className="nav-item">
                 <Link to="/" className="nav-link">
-                  About
+                  Budgeting
                 </Link>
               </li>
               <li className="nav-item">
@@ -43,14 +42,85 @@ export default function Navbar() {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" id="menu-bar">
-                  <FontAwesomeIcon icon={faBars} />
+                <Link to="/" className="nav-link">
+                  Wealth creation
                 </Link>
+              </li>
+            </ul>
+            <hr />
+            <ul className="social mb-0">
+              <li>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+              </li>
+              <li>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </li>
+              <li>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </li>
+              <li>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </div>
-    </nav>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            {/* <img
+            src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png"
+            alt="Logo"
+          /> */}
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+            <div className="d-flex">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Start Here
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Contact
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" id="menu-bar">
+                    <FontAwesomeIcon icon={faBars} />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
