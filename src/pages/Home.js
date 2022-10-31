6dc374375217c0b99a5d693ef7e4f8b00098eb1e
Wilson Kinyua <wilsonkinyuam@gmail.com>
2022-10-31 18:29:16 +0300

    welcome message from founder updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import React from "react";
 // import landing from "../img/Figuring Finance Landing.png"
 // import owner from "../img/2.jpeg";
 import journey from "../img/journey.jpeg";
+import support from "../icons/support.png";
+import correct from "../icons/correct.png";
+import brave from "../icons/protest.png";
+import investments from "../icons/investments-accesibility.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
@@ -81,7 +85,7 @@ export default function App() {
                 you are and where to go on your journey to Financial
                 Independence.
               </p>
-              <Link className="btn btn-primary btn-lg mt-3">
+              <Link className="btn btn-primary btn-lg mt-3 btn-bg-secondary">
                 Read more{" "}
                 <span>
                   <FontAwesomeIcon icon={faArrowRight} />
@@ -102,37 +106,67 @@ export default function App() {
         </div>
       </section>
       <section className="journey">
-        <div className="row">
+        <div className="row" style={{ marginRight: "20px" }}>
           <div className="col-md-5">
             <div className="img-wrapper">
               <img src={journey} alt="Journey" />
             </div>
           </div>
           <div className="col-md-7">
-            <div className="journey-content">
-              <h5 className="text-uppercase">from our founder</h5>
-              <h2>Welcome To Figuring Finance</h2>
-              <p>
-                I love to teach individuals how to do better with their money,
-                to inspire them to take that bold step and ignite your financial
-                freedom journey.
-              </p>
-              <p>
-                In a dark time in my life I found myself wondering where all my
-                money was going, I could barely save, did not understand the
-                importance of investing, felt stuck and overwhelmed. This led me
-                to invest in my own personal finance education, grow my savings
-                and investment portfolio. What began as a personal journey
-                towards financial stability for my daughter and I was a catalyst
-                for helping others take control of their personal finances and
-                build the life they want to live
-              </p>
-              <Link className="btn btn-primary btn-lg">
-                Read more{" "}
-                <span>
-                  <FontAwesomeIcon icon={faArrowRight} />
+            <div className="row">
+              <div className="col-md-12">
+                <div className="journey-content">
+                  <h5 className="text-uppercase">from our founder</h5>
+                  <h2 className="mt-4 mb-4">Welcome To Figuring Finance</h2>
+                  <p>
+                    I love to teach individuals how to do better with their
+                    money, to inspire them to take that bold step and ignite
+                    your financial freedom journey.
+                  </p>
+                  <p>
+                    In a dark time in my life I found myself wondering where all
+                    my money was going, I could barely save, did not understand
+                    the importance of investing, felt stuck and overwhelmed.
+                    This led me to invest in my own personal finance education,
+                    grow my savings and investment portfolio. What began as a
+                    personal journey towards financial stability for my daughter
+                    and I was a catalyst for helping others take control of
+                    their personal finances and build the life they want to live
+                  </p>
+                  <Link className="btn btn-primary btn-lg mt-3">
+                    Read more{" "}
+                    <span>
+                      <FontAwesomeIcon icon={faArrowRight} />
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="row text-center icons g-0">
+              <div className="col-md-3">
+                <span className="icon-wrapper">
+                  <img src={support} alt="Support" />
+                  <h5>Support</h5>
                 </span>
-              </Link>
+              </div>
+              <div className="col-md-3">
+                <span className="icon-wrapper">
+                  <img src={correct} alt="Sound Financial Education" />
+                  <h5>Sound Financial Education</h5>
+                </span>
+              </div>
+              <div className="col-md-3">
+                <span className="icon-wrapper">
+                  <img src={brave} alt="Brave" />
+                  <h5>Brave</h5>
+                </span>
+              </div>
+              <div className="col-md-3">
+                <span className="icon-wrapper">
+                  <img src={investments} alt="Accessibility" />
+                  <h5>Accessibility</h5>
+                </span>
+              </div>
             </div>
           </div>
         </div>
